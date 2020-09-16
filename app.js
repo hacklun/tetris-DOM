d5104bec4006762c023e5fee072b2c8f8c3fa6f5
hacklun <hacklun@mail.ru>
2020-09-16 11:21:48 +0300

    add zTetromino

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10;
     const height = 20;
 
-    const l Tetromino = [
+    const lTetromino = [
         [1, width + 1, width * 2 + 1, 2],
-        
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+    const zTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
     ];
 })
