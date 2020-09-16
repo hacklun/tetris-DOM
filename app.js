3a4e2b501d9577a2c686aca10b8f4a49bf2ed05d
hacklun <hacklun@mail.ru>
2020-09-16 10:55:55 +0300

    start Tetromino

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
     const width = 10;
     const height = 20;
+
+    const l Tetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        
+    ];
 })
